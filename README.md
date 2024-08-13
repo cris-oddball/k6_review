90b15ba602d16e2a0a0d29d2a0453557630df72c
cris-oddball <cris.stoddard@oddball.io>
2024-08-13 14:43:38 -0600

    finish up poc

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ Overview of using K6 for notification load testing
 4. To run the open model test, which decouples the number of requests from the time it takes to get back a response from the first request, run ` k6 run open-model.js `
 
 Read more at [Open and closed models](https://grafana.com/docs/k6/latest/using-k6/scenarios/concepts/open-vs-closed/) to better understand the difference between these tests. It is a differentiator for using K6.
+
+
+The email load test in  `email-load-test.js`, successfully generates a bearer token but returns a 403 
+```
+{"status_code":403,"errors":[{"error":"AuthError","message":"Invalid token: signature, api token is not valid"}]}
+```
+
+If you want to experiement with this test, run the following command, supplying the environment variables:
+```
+k6 run email-load-test.js --env API_KEY='<a perf api key>' --env SERVICE_ID='<a perf service id>' --env EMAIL_ADDRESS='<an email address>' --env EMAIL_TEMPLATE_ID='<a perf template id in the service>'
+```
+
+Note that something like dotenv cannot be used in k6. There are other options for passing in configs in k6, but this was the most straightfoward way for a POC.
--- email-load-test.js
+++ email-load-test.js
@@ -1,5 +1,24 @@
+import crypto from 'k6/crypto';
+import encoding from 'k6/encoding';
 import http from 'k6/http';
 import { sleep } from 'k6';
+
+console.log(__ENV.SERVICE_ID) // remove this after you've confirmed it is working
+console.log(__ENV.API_KEY) // remove this after you've confirmed it is working
+
+const url = 'https://sandbox-api.va.gov/vanotify/v2/notifications/email'
+
+// Create a JWT token
+function createJWT(header, payload, secret) {
+  const encodedHeader = encoding.b64encode(JSON.stringify(header), 'url');
+  const encodedPayload = encoding.b64encode(JSON.stringify(payload), 'url');
+  
+  const token = `${encodedHeader}.${encodedPayload}`;
+  const signature = crypto.hmac('sha256', secret, token, 'hex');
+  
+  return `${token}.${encoding.b64encode(signature, 'url')}`;
+}
+
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
@@ -8,12 +27,49 @@ export const options = {
   duration: '1s',
 };
 
+export function setup() {
+  const serviceId = __ENV.SERVICE_ID;
+  const apiKey = __ENV.API_KEY;
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+
+  const header = {
+      typ: 'JWT',
+      alg: 'HS256',
+  };
+
+  const payload = {
+      iss: serviceId,
+      iat: currentTimestamp,
+      exp: currentTimestamp + 30,
+      jti: 'jwt_nonce',
+  };
+
+  const jwtToken = createJWT(header, payload, apiKey);
+  console.log('Generated JWT:', jwtToken); // remove this after you've confirmed it is working
+  return jwtToken
+}
+
 // The function that defines VU logic.
 //
 // See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
 // about authoring k6 scripts.
 //
-export default function() {
-  http.get('https://test.k6.io');
+export default function(authToken) {
+
+  console.log(authToken)
+  const templateId = __ENV.EMAIL_TEMPLATE_ID
+  const emailAddress = __ENV.EMAIL_ADDRESS
+
+  let data = { 
+    template_id: templateId,
+    email_address: emailAddress
+  };
+
+  let res = http.post(url, JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ${authToken}' },
+  });
+  console.log(res.json())
+
+  // http.get('https://test.k6.io');
   sleep(1);
 }
